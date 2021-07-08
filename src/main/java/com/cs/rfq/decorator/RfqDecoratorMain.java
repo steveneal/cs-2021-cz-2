d3c64166fb2902c3f0d9780c88953e11df2a6b04
fredbaos <myottef@gmail.com>
2021-07-08 06:17:54 -0400

    test first commit

--- src/main/java/com/cs/rfq/decorator/RfqDecoratorMain.java
+++ src/main/java/com/cs/rfq/decorator/RfqDecoratorMain.java
@@ -1,6 +1,7 @@
 package com.cs.rfq.decorator;
 
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.streaming.Durations;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
@@ -12,12 +13,19 @@ public class RfqDecoratorMain {
         System.setProperty("spark.master", "local[4]");
 
         //TODO: create a Spark configuration and set a sensible app name
+        SparkConf conf = new SparkConf().setAppName("RfqDecorator");
 
         //TODO: create a Spark streaming context
+        JavaStreamingContext jssc = new JavaStreamingContext(conf, Durations.seconds(1));
 
         //TODO: create a Spark session
+        SparkSession session = SparkSession.builder()
+                .appName("RfqDecorator")
+                .getOrCreate();
 
         //TODO: create a new RfqProcessor and set it listening for incoming RFQs
+        
+
     }
 
 }
