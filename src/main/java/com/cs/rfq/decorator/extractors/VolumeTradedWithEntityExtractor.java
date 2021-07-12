a5ec2e0904652f2bd56c7d1dd8ad0d64e628c3ab
Fabio Guerra <guerra.fabio.1998@gmail.com>
2021-07-12 11:00:30 -0400

    Tweak sample data for demo

--- src/main/java/com/cs/rfq/decorator/extractors/VolumeTradedWithEntityExtractor.java
+++ src/main/java/com/cs/rfq/decorator/extractors/VolumeTradedWithEntityExtractor.java
@@ -8,11 +8,8 @@ import org.apache.spark.sql.SparkSession;
 import java.util.Map;
 
 public abstract class VolumeTradedWithEntityExtractor extends VolumeTradedByBase {
-
     @Override
     public Map<RfqMetadataFieldNames, Object> extractMetaData(Rfq rfq, SparkSession session, Dataset<Row> trades) {
-        System.out.println(since);
-        System.out.println(until);
         String query = String.format("SELECT sum(LastQty) from trade where EntityId='%s' AND SecurityId='%s' AND TradeDate >= '%s' AND TradeDate <= '%s'",
                 rfq.getEntityId(),
                 rfq.getIsin(),
