3049cc8bd414cf25ddb94126e62f349a74dd4e1f
Fabio Guerra <guerra.fabio.1998@gmail.com>
2021-07-12 02:36:54 -0400

    Refactor publishing mechanism
    Refactor publishing mechanism to use separate class from RfqProcessor.

--- src/main/java/com/cs/rfq/decorator/publishers/MetadataJsonLogPublisher.java
+++ src/main/java/com/cs/rfq/decorator/publishers/MetadataJsonLogPublisher.java
@@ -1,6 +1,7 @@
 package com.cs.rfq.decorator.publishers;
 
 import com.cs.rfq.decorator.extractors.RfqMetadataFieldNames;
+import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,6 +11,12 @@ import java.util.Map;
 public class MetadataJsonLogPublisher implements MetadataPublisher {
 
     private static final Logger log = LoggerFactory.getLogger(MetadataJsonLogPublisher.class);
+
+    private final Gson gson;
+
+    public MetadataJsonLogPublisher() {
+        gson = new GsonBuilder().setPrettyPrinting().create();
+    }
 
     @Override
     public void publishMetadata(Map<RfqMetadataFieldNames, Object> metadata) {
