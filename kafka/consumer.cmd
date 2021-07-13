6ea6f1d36f0f3bd32270abcc4c8eef51b2740d18
Fabio Guerra <guerra.fabio.1998@gmail.com>
2021-07-13 03:07:45 -0400

    Adjust consumer.cmd utility script

--- kafka/consumer.cmd
+++ kafka/consumer.cmd
@@ -1,1 +1,1 @@
-bin\windows\kafka-console-consumer.bat --topic rfq-metadata --from-beginning --bootstrap-server localhost:9092
+bin\windows\kafka-console-consumer.bat --topic rfq-metadata --bootstrap-server localhost:9092
